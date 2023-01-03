6a4095660ce13d45da816919ed3183231f62b2fe
Chitrarekha  Kursunge <87944602+ChitrarekhaKursunge@users.noreply.github.com>
2023-01-03 21:03:08 +0530

    Pyramid Pattern
             *
           * * *
         * * * * *
      * * * * * * *
    * * * * * * * * *

---
+++ PyramidPAttern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class PyramidPAttern
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter no. of rows");
+		int row=sc.nextInt();
+		System.out.println("enter no. of columns");
+		int column=sc.nextInt();
+		for(int i=1; i<=row; i++)
+		{
+			for(int j=1; j<=column; j++)
+			{
+				if(i+j>=column+1)
+				{
+					System.out.print("*" +" ");
+				}
+				else
+				{
+					System.out.print(" " +" ");
+				}
+			}
+			for(int k=1; k<i; k++)
+			{
+				System.out.print("*" +" ");
+			}
+			System.out.println(" ");
+		}		
+	}
+}
